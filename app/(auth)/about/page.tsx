54a320d0b6d183d9bb9c4c5d92a9250ae3dd2c5f
Laila Roikha <lailajnnh@gmail.com>
2024-05-19 23:52:01 +0700

    theme-about us

--- app/(auth)/about/page.tsx
+++ app/(auth)/about/page.tsx
@@ -19,7 +19,7 @@ import Link from 'next/link'
       heightFix()
     }, []) 
     return (
-      <section className="bg-gradient-to-b from-gray-100 to-white">
+      <section className="">
         <div className="max-w-6xl mx-auto px-4 sm:px-6">
           <div className="pt-32 pb-10 md:pt-40 md:pb-20">
 
--- app/(auth)/contact/page.tsx
+++ app/(auth)/contact/page.tsx
@@ -4,7 +4,7 @@ import FeaturesBg from '@/public/images/features-bg.png'
 
 export default function contact() {
     return (
-        <section className="bg-gradient-to-b from-gray-100 to-white">
+        <section className="">
           <div className="max-w-6xl mx-auto px-4 sm:px-6">
             <div className="pt-32 pb-12 md:pt-40 md:pb-20">
                 {/* Section header */}
