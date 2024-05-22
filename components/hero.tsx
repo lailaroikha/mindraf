d0dc5f0b9e426b7863d5020ee1de50534789317b
Laila Roikha <lailajnnh@gmail.com>
2024-05-22 13:54:12 +0700

    foto1

--- components/hero.tsx
+++ components/hero.tsx
@@ -19,13 +19,6 @@ export default function Hero() {
                 <TypeAnimation sequence={["Hi, Kha Journey:  Empowering Education! ", "Hi, Kha Journey: Your Homework Helper! "]} delay={3000} />
                 </p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
-
-                <div>
-                  <a className="btn text-white bg-blue-700 hover:bg-blue-900 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start now</a>
-                </div>
-                <div>
-                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
-                </div>
               </div>
             </div>
           </div>         
