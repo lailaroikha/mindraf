f16f57e572f9ba8003e91254fdbdee807c0747d3
Laila Roikha <lailajnnh@gmail.com>
2024-05-22 13:30:15 +0700

    foto

--- components/newsletter.tsx
+++ components/newsletter.tsx
@@ -10,13 +10,6 @@ export default function Newsletter() {
                 <div className="text-center lg:text-left lg:max-w-xl">
                   <h3 className="h3 text-white mb-2">Stay Updated with Kha Journey!</h3>
                   <p className="text-gray-300 text-lg mb-6">Be the first to know about our latest adventures, exclusive content, and special offers. Subscribe now to stay connected!</p>
-                  <form className="w-full lg:w-auto">
-                    <div className="flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-md lg:mx-0">
-                      <input type="email" className="form-input w-full appearance-none bg-gray-800 border border-gray-700 focus:border-gray-600 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-500" placeholder="Your email…" aria-label="Your email…" />
-                      <a className="btn text-white bg-blue-600 hover:bg-blue-700 shadow" href="#0">Subscribe</a>
-                    </div>
-                    <p className="text-sm text-gray-400 mt-3">No spam. You can unsubscribe at any time.</p>
-                  </form>
                 </div>
   
               </div>
