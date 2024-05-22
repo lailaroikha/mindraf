455b4e156709db4a1ef8e9e88c806e54c5b8a37c
Laila Roikha <lailajnnh@gmail.com>
2024-05-22 13:11:25 +0700

    foto

--- components/features-blocks.tsx
+++ components/features-blocks.tsx
@@ -31,79 +31,30 @@ export default function FeaturesBlocks() {
   
               {/* 2nd item */}
               <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
-                <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                  <g fill="none" fillRule="evenodd">
-                    <rect className="fill-current  text-blue-950" width="64" height="64" rx="32" />
-                    <g strokeWidth="2" transform="translate(19.429 20.571)">
-                      <circle className="stroke-current text-white" strokeLinecap="square" cy="12.571" r="1.143" />
-                      <path className="stroke-current text-white" d="M19.153 23.267c3.59-2.213 5.99-6.169 5.99-10.696C25.143 5.63 19.514 0 12.57 0 5.63 0 0 5.629 0 12.571c0 4.527 2.4 8.483 5.99 10.696" />
-                      <path className="stroke-current text-blue-300" d="M16.161 18.406a6.848 6.848 0 003.268-5.835 6.857 6.857 0 00-6.858-6.857 6.857 6.857 0 00-6.857 6.857 6.848 6.848 0 003.268 5.835" />
-                    </g>
-                  </g>
-                </svg>
                 <h4 className="text-gray-800 text-xl font-bold leading-snug tracking-tight mb-1">Textbook explanations</h4>
                 <p className="text-gray-600 text-center">Read the book & still confused? Get detailed, step-by-step breakdowns of complicated textbook problems instantly online.</p>
               </div>
   
               {/* 3rd item */}
               <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
-                <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                  <g fill="none" fillRule="evenodd">
-                    <rect className="fill-current  text-blue-950" width="64" height="64" rx="32" />
-                    <g strokeWidth="2">
-                      <path className="stroke-current text-blue-300" d="M34.743 29.714L36.57 32 27.43 43.429H24M24 20.571h3.429l1.828 2.286" />
-                      <path className="stroke-current text-white" strokeLinecap="square" d="M34.743 41.143l1.828 2.286H40M40 20.571h-3.429L27.43 32l1.828 2.286" />
-                      <path className="stroke-current text-blue-300" d="M20.571 32H40" />
-                      <path className="stroke-current text-white" d="M24 32h3.429" strokeLinecap="square" />
-                    </g>
-                  </g>
-                </svg>
                 <h4 className="text-gray-800 text-xl font-bold leading-snug tracking-tight mb-1">Understand a topic</h4>
                 <p className="text-gray-600 text-center">Simplify the toughest concepts with digestible topic breakdowns.</p>
               </div>
   
               {/* 4th item */}
               <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
-                <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                  <g fill="none" fillRule="evenodd">
-                    <rect className="fill-current text-blue-950" width="64" height="64" rx="32" />
-                    <g strokeWidth="2">
-                      <path className="stroke-current text-white" d="M32 37.714A5.714 5.714 0 0037.714 32a5.714 5.714 0 005.715 5.714" />
-                      <path className="stroke-current text-white" d="M32 37.714a5.714 5.714 0 015.714 5.715 5.714 5.714 0 015.715-5.715M20.571 26.286a5.714 5.714 0 005.715-5.715A5.714 5.714 0 0032 26.286" />
-                      <path className="stroke-current text-white" d="M20.571 26.286A5.714 5.714 0 0126.286 32 5.714 5.714 0 0132 26.286" />
-                      <path className="stroke-current text-blue-300" d="M21.714 40h4.572M24 37.714v4.572M37.714 24h4.572M40 21.714v4.572" strokeLinecap="square" />
-                    </g>
-                  </g>
-                </svg>
                 <h4 className="text-gray-800 text-xl font-bold leading-snug tracking-tight mb-1">Homework</h4>
                 <p className="text-gray-600 text-center">84+ million step-by-step explanations, Expert Q&A and math support 24/7.</p>
               </div>
   
               {/* 5th item */}
               <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
-                <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                  <g fill="none" fillRule="evenodd">
-                    <rect className="fill-current text-blue-950" width="64" height="64" rx="32" />
-                    <g strokeWidth="2">
-                      <path className="stroke-current text-blue-300" d="M32 20.429c6.943 0 12.571 5.628 12.571 12.571M32 24a8 8 0 018 8" />
-                    </g>
-                  </g>
-                </svg>
                 <h4 className="text-gray-800 text-xl font-bold leading-snug tracking-tight mb-1">Writing</h4>
                 <p className="text-gray-600 text-center">Get help on your paper from start to finish with our suite of writing tools.</p>
               </div>
   
               {/* 6th item */}
               <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
-                <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                  <g fill="none" fillRule="evenodd">
-                    <rect className="fill-current text-blue-950" width="64" height="64" rx="32" />
-                    <g strokeWidth="2" strokeLinecap="square">
-                      <path className="stroke-current text-white" d="M29.714 40.358l-4.777 2.51 1.349-7.865-5.715-5.57 7.898-1.147L32 21.13l3.531 7.155 7.898 1.147L40 32.775" />
-                      <path className="stroke-current text-blue-300" d="M44.571 43.429H34.286M44.571 37.714H34.286" />
-                    </g>
-                  </g>
-                </svg>
                 <h4 className="text-gray-800 text-xl font-bold leading-snug tracking-tight mb-1">Math Solver</h4>
                 <p className="text-gray-600 text-center">Understand math, one step at a time. Detailed explanation with sub-steps</p>
               </div>
