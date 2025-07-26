559183decad8d1aab7694fa47b9bc3ebb39fac56
lailaroikha <lailajnnh@gmail.com>
2025-07-26 21:21:38 +0700

    edit isi

--- components/hero.tsx
+++ components/hero.tsx
@@ -11,12 +11,12 @@ export default function Hero() {
 
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out"> Study Seamlessly with</h1>
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out"> <span className="bg-clip-text text-transparent bg-gradient-to-r from-gray-900 to-gray-500"> Kha Journey </span></h1>
+            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out"> Mindraft </h1>
+            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out"> <span className="bg-clip-text text-transparent bg-gradient-to-r from-gray-900 to-gray-500"> Platform Belajar dan Berkarya Ilmiah </span></h1>
             <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-4" data-aos="zoom-y-out" data-aos-delay="150">Solutions backed by verified experts. Specialized tools to help master your courses. Tailored to support the way you learn</p>
+              <p className="text-xl text-gray-600 mb-4" data-aos="zoom-y-out" data-aos-delay="150"> Mindraft menghadirkan semangat, motivasi, dan ruang diskusi yang mendorong pertumbuhan dan pencapaian akademik </p>
               <p className="text-center font-sora mb-8 text-1xl font-medium lg:text-3xl">
-                <TypeAnimation sequence={["Hi, Kha Journey:  Empowering Education! ", "Hi, Kha Journey: Your Homework Helper! "]} delay={3000} />
+                <TypeAnimation sequence={["Hi, Kha Journey:  Empowering Education! ", "Mindraft! Naikin Semangat, Buka Peluang! "]} delay={3000} />
                 </p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
               </div>
