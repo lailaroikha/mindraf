6c2d619d2618fdb039f8b4dd605105539a506fcd
lailaroikha <lailajnnh@gmail.com>
2025-07-26 22:15:45 +0700

    logo

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -1,5 +1,4 @@
-import Image from 'next/image'
-import TestimonialImage from '@/public/images/testimonial.jpg'
+
 
 export default function Testimonials() {
   return (
--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,16 +1,29 @@
+// import Link from 'next/link'
+// import Image from 'next/image'
+// import React from 'react'
+// import IconLogo from '@/public/images/logokhajourney.webp'
+
+// const Logo: React.FC = () => {
+//   return (
+//     <Link href="/" aria-label="Cruip">
+//       <Image className='w-20 h-20'
+//         src={IconLogo} 
+
+//         alt="Kha Journey"
+//       />
+//     </Link>
+//   )
+// }
+
+// export default Logo
+
 import Link from 'next/link'
-import Image from 'next/image'
 import React from 'react'
-import IconLogo from '@/public/images/logokhajourney.webp'
 
 const Logo: React.FC = () => {
   return (
-    <Link href="/" aria-label="Cruip">
-      <Image className='w-20 h-20'
-        src={IconLogo} 
-
-        alt="Kha Journey"
-      />
+    <Link href="/" aria-label="Mindraft">
+      <span className="text-3xl font-bold tracking-tight text-[#0A2A42]">Mindraft</span>
     </Link>
   )
 }
